4aec82f3c07663f7c92d9a50d229cde8137a823d
Rui Qin Ng <ngruiqin_1994@hotmail.com>
2023-04-05 00:21:15 +0800

    Refactor model and fix bug in concat

--- scripts/model.py
+++ scripts/model.py
@@ -1,6 +1,22 @@
+from collections.abc import Sequence
 from torch import nn
 from torchvision.models import mobilenet_v3_large, MobileNet_V3_Large_Weights
 import torch
+
+
+class SiameseNetwork(nn.Module):
+    def __init__(self, net1: nn.Module, net2: nn.Module) -> None:
+        super().__init__()
+        self.net1 = net1
+        self.net2 = net2
+
+
+    def forward(self, images: tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
+        img1, img2 = images
+        output_1, output_2 = self.net1(img1), self.net2(img2)
+        output = torch.cat((output_1, output_2), dim=1)
+        return output
+        
 
 class SiameseDuplicateImageNetwork(nn.Module):
     def __init__(self):
@@ -9,16 +25,16 @@ class SiameseDuplicateImageNetwork(nn.Module):
         self.preprocess = weights.transforms()
         
         # Instantiate MobileNetV3, and load pre-trained weights
-        self.network_1 = mobilenet_v3_large(weights=weights)
-        self.network_2 = mobilenet_v3_large(weights=weights)
+        mobilenetv3_1 = mobilenet_v3_large(weights=weights)
+        mobilenetv3_2 = mobilenet_v3_large(weights=weights)
         
         # Freeze MobileNetV3 models
-        for param in self.network_1.parameters():
+        for param in mobilenetv3_1.parameters():
             param.requires_grad_(False)
-        for param in self.network_2.parameters():
+        for param in mobilenetv3_2.parameters():
             param.requires_grad_(False)
 
-        self.duplicate_image_classifier = nn.Sequential(
+        duplicate_image_classifier = nn.Sequential(
             nn.Linear(1000 * 2, 2048),
             nn.ReLU(),
             nn.Dropout(0.2),
@@ -39,19 +55,20 @@ class SiameseDuplicateImageNetwork(nn.Module):
             
             nn.Linear(64, 1)
         )
+
+        self.model = nn.Sequential(
+            SiameseNetwork(mobilenetv3_1, mobilenetv3_2),
+            duplicate_image_classifier
+        )
             
 
-    def forward(self, img_1, img_2):
+    def forward(self, img_1: torch.Tensor, img_2: torch.Tensor) -> torch.Tensor:
         # Preprocess images
         img1 = self.preprocess(img_1)
         img2 = self.preprocess(img_2)
 
         # Pass through Siamese network
-        model_1_output = self.network_1(img1)
-        model_2_output = self.network_2(img2)
-
-        output_concat = torch.cat((model_1_output, model_2_output), 1)
-        output = self.duplicate_image_classifier(output_concat)
+        output = self.model((img1, img2))
         return output
 
 
